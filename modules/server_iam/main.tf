995efad5e1d4ea486ed42fb7f72f10c8c8dbce9f
Zeus Gomez Marmolejo <zeos@google.com>
2019-11-21 15:23:24 +0100

    Remove folder_read permissions to avoid errors

--- modules/server_iam/main.tf
+++ modules/server_iam/main.tf
@@ -72,13 +72,6 @@ resource "google_organization_iam_member" "org_read" {
   member = "serviceAccount:${google_service_account.forseti_server.email}"
 }
 
-resource "google_folder_iam_member" "folder_read" {
-  count  = var.folder_id != "" ? length(local.server_read_roles) : 0
-  role   = local.server_read_roles[count.index]
-  folder = var.folder_id
-  member = "serviceAccount:${google_service_account.forseti_server.email}"
-}
-
 resource "google_organization_iam_member" "org_write" {
   count  = var.org_id != "" && var.enable_write ? length(local.server_write_roles) : 0
   role   = local.server_write_roles[count.index]
